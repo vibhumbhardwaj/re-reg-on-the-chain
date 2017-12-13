16714fb4b46374f548a17a596a8e2d707c56bf02
vibhumbhardwaj <vibhumbhardwaj@hotmail.com>
2017-12-13 10:09:25 -0500

    Enabled CORS access

--- api/config/express.js
+++ api/config/express.js
@@ -1,9 +1,24 @@
 import bodyParser from 'body-parser';
+import { environment } from './config';
+
+var configCORS = (req, res, next) => {
+  
+  if(environment == 'local') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  }
+  // else dont allow with eyes closed.
+  
+  return next();
+}
+
 
 export default (app) => {
-    app.set('port', (process.env.PORT || 8080));
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
-    app.use(bodyParser.json());
+  app.set('port', (process.env.PORT || 8080));
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+  app.use(bodyParser.json());
+  app.use(configCORS);
 };
